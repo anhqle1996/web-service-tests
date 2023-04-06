07562131e37249f55426d7d22b3a0c135da5cc39
Anh Le <47583931+anhqle1996@users.noreply.github.com>
2023-04-06 14:15:43 +0700

    Katalon: Update test suite New in test-suites/test/1234

--- test-suites/test/1234/New.ts
+++ test-suites/test/1234/New.ts
@@ -1,13 +1,17 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>New</name>
-    <testSuiteGuid>fa8ead93-b328-42db-96ed-5e02fecf3cef</testSuiteGuid>
+    <testSuiteGuid>e2ccc18e-e419-4f89-91fc-40adf3326390</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/G5</testCaseId>
-        <guid>f5951bdb-4189-4ae3-8ce5-306be2a0b77b</guid>
+        <guid>2bc9f886-6435-4307-8102-2dd55f4f1bff</guid>
     </testCaseLink>
     <testCaseLink>
-        <testCaseId>test-cases/G5</testCaseId>
-        <guid>10c60778-2b85-47b7-b70b-04aa2f625953</guid>
+        <testCaseId>test-cases/123/Test Case G5</testCaseId>
+        <guid>cdfc1191-d8fd-49ba-96ba-7113c3a1532b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/G5 (2)</testCaseId>
+        <guid>b7af155c-7da3-42fc-b7b1-400b056d1fe3</guid>
     </testCaseLink>
 </TestSuiteEntity>
