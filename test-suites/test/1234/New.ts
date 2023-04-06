219c9080760a91ba4f2043c0461133ba8c5282ed
Anh Le <47583931+anhqle1996@users.noreply.github.com>
2023-04-06 14:05:20 +0700

    1231321

---
+++ test-suites/test/1234/New.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>New</name>
+    <testSuiteGuid>fa8ead93-b328-42db-96ed-5e02fecf3cef</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/G5</testCaseId>
+        <guid>f5951bdb-4189-4ae3-8ce5-306be2a0b77b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/G5</testCaseId>
+        <guid>10c60778-2b85-47b7-b70b-04aa2f625953</guid>
+    </testCaseLink>
+</TestSuiteEntity>
