b3cbb61a59b7fa0143fbf094891a5b093fe9647d
Anh Le <47583931+anhqle1996@users.noreply.github.com>
2023-04-07 14:11:30 +0700

    A commit

---
+++ test-suites/test/Anh Le.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Anh Le</name>
+    <testSuiteGuid>44ff4df8-843d-432f-b2f3-be927e2322e3</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/123/Test Case G5</testCaseId>
+        <guid>00c5fdf2-139f-4673-b690-53417b6b4880</guid>
+    </testCaseLink>
+</TestSuiteEntity>
