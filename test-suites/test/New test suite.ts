7352701e68d322327563e3cc8596a620667f6354
Anh Le <47583931+anhqle1996@users.noreply.github.com>
2023-04-17 09:43:44 +0700

    a commit

---
+++ test-suites/test/New test suite.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>New test suite</name>
+    <testSuiteGuid>0e556697-7efd-44c2-b16f-34f0c082dbd7</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/G5</testCaseId>
+        <guid>b21929e0-d0d4-467f-848e-c3ff91a026c7</guid>
+    </testCaseLink>
+</TestSuiteEntity>
