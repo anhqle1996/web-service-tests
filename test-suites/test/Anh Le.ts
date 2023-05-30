bbb64a4d5258e5e994dd969a0e7be5e2402e0346
Anh Le <47583931+anhqle1996@users.noreply.github.com>
2023-05-30 11:39:27 +0700

    Katalon: Update test suite Anh Le in test-suites/test

--- test-suites/test/Anh Le.ts
+++ test-suites/test/Anh Le.ts
@@ -1,9 +1,21 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>Anh Le</name>
-    <testSuiteGuid>44ff4df8-843d-432f-b2f3-be927e2322e3</testSuiteGuid>
+    <testSuiteGuid>3cc32021-cc49-4314-ac97-c87c29d809d4</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/123/Test Case G5</testCaseId>
-        <guid>00c5fdf2-139f-4673-b690-53417b6b4880</guid>
+        <guid>b8b170fa-8434-47d8-85bf-d6540a66319a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/123/Test Case G5</testCaseId>
+        <guid>8374eb30-8946-49b0-9ed4-8b895ae81044</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/G5</testCaseId>
+        <guid>55af484d-868d-4845-a6e4-930a8607496e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/G5 (2)</testCaseId>
+        <guid>c4400a61-1a77-4447-94e4-4dff655db205</guid>
     </testCaseLink>
 </TestSuiteEntity>
